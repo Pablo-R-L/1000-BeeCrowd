c48f48f3eeda3513251e076ef917c4752504f68a
Pablo-R-L <99687142+Pablo-R-L@users.noreply.github.com>
2022-02-16 10:51:16 -0300

    Add files via upload

--- AULA - Ex.cpp
+++ AULA - Ex.cpp
@@ -1,10 +1,6 @@
 #include<stdio.h>
-#include<chrono>
-#include <thread>
 int main(){//n muda
-	using namespace std::this_thread; // sleep_for, sleep_until
-    using namespace std::chrono;
-/*
+
 	double X, Y, Res;
 	
 	printf("Insira valor de A e B: ");
@@ -24,11 +20,8 @@ int main(){//n muda
 	
 	
 	Res = X/Y;
-	printf("%.1lf / %.1lf = %.1lf \n", X, Y, Res);*/
+	printf("%.1lf / %.1lf = %.1lf \n", X, Y, Res);
 	
-	for (int i; i<3; i++){
-		printf("pog\n" );
-		sleep_for(nanoseconds(100000));
-	}
+
 	
 }
